00cc9d200b8151914cb4dc095b2a0aae678bde2b
abulfazabbasli <abulfazabbasli@localhost>
2020-08-19 01:54:31 +0300

    First Commit

---
+++ IssueManagment/src/Main/Spike.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class Spike {
+
+	public static void main(String[] args) {
+		String s = "/System/Sub";
+		
+		System.out.println("'" + s.replaceAll("/[^/]+$","")+"'");
+
+	}
+
+}
